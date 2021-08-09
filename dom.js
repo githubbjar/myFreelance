e11d2da36d0cbf5b5bca9790065641988ae0cc59
janQuart <janquart@fsj.org>
2021-08-09 11:30:53 -0500

    syncing code

--- dom.js
+++ dom.js
@@ -19,7 +19,7 @@ document.getElementById("job-list").innerHTML = "<p><strong>Jobs:</strong></p>" 
 let jobStatus = "";
 //completed, billed & paid
 if (jobs[x].completed_date && jobs[x].date_billed && jobs[x].date_paid) {
-    jobStatus = "<div class='green'><strong>JOB STATUS: </strong> &#8212;> <i class='fas fa-check-square'></i> Completed on " + jobs[x].completed_date + " &#8212;> <i class='fas fa-check-square'></i> Billed on " + jobs[x].date_billed + " &#8212> <i class='fas fa-check-square'></i> Payment Recieved on " + jobs[x].date_paid + "</div>"; 
+    jobStatus = "<div class='green'><strong>JOB STATUS: </strong> &#8212;><i class='fas fa-check-square'></i> Completed on " + jobs[x].completed_date + " &#8212;> <i class='fas fa-check-square'></i> Billed on " + jobs[x].date_billed + " &#8212> <i class='fas fa-check-square'></i> Payment Recieved on " + jobs[x].date_paid + "</div>"; 
 // completed & billed
 } else if (jobs[x].completed_date && jobs[x].date_billed && !jobs[x].date_paid) {
     jobStatus = "<div class='yellow'><strong>JOB STATUS: </strong> &#8212;> <i class='fas fa-check-square'></i> Completed on " + jobs[x].completed_date + " &#8212;> <i class='fas fa-check-square'></i> Billed on " + jobs[x].date_billed + " &#8212> <i class='far fa-square'></i> Awaiting payment . . .</div>"
