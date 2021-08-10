b0d4c2b66c598b8a2bae2b2fa13d4a778ba0af6e
janQuart <janquart@fsj.org>
2021-08-10 10:53:29 -0500

    update address and contacts for Biretta and PMA

--- job-list.js
+++ job-list.js
@@ -9,29 +9,29 @@ function Job(invoice_number, job_number, employer, project, project_type, start_
     this.job_number = job_number;
     this.employer = employer;
     this.contact = function() {
-        if (this.employer == "St. John Cantius Church") {
+        if (this.employer == "Biretta Books") {
             return "Nick Chapello";
         } else if (this.employer == "Publishing Management Associates") {
-            return "Sue Milnes";
+            return "Richard A. Vaughan";
         }
     };
     this.contact_email = function() {
-        if (this.employer == "St. John Cantius Church") {
+        if (this.employer == "Biretta Books") {
             return "nchapello@cantius.org";
         } else if (this.employer == "Publishing Management Associates") {
-            return "sue@pma-inc.net";
+            return "ravaughan@pma-inc.net";
         }
     };
     this.employer_address1 = function() {
-        if (this.employer == "St. John Cantius Church") {
-            return "825 N. Carpenter Street";
+        if (this.employer == "Biretta Books") {
+            return "1025 West Fry Street";
         } else if (this.employer == "Publishing Management Associates") {
             return "129 Phelps Avenue, Suite 312";
         }
     };
     this.employer_address2 = function() {
-        if (this.employer == "St. John Cantius Church") {
-            return "Chicago, IL 60642";
+        if (this.employer == "Biretta Books") {
+            return "Chicago, IL 60642-5419";
         } else if (this.employer == "Publishing Management Associates") {
             return "Rockford, IL 61108";
         }
@@ -41,8 +41,8 @@ function Job(invoice_number, job_number, employer, project, project_type, start_
     this.start_date = start_date;
     this.due_date = due_date;
     this.employer_nickname = function() {
-        if (this.employer == "St. John Cantius Church") {
-            return "Cantius";
+        if (this.employer == "Biretta Books") {
+            return "Biretta";
         } else if (this.employer == "Publishing Management Associates") {
             return "PMA";
         }
@@ -56,7 +56,7 @@ function Job(invoice_number, job_number, employer, project, project_type, start_
 
 //the jobs
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-const job2708 = new Job(2708, 0, "St. John Cantius Church", "2021 Music Schedule", "16 page booklet, 5.5 x 8.5", "July 17, 2021", "August 6, 2021");
+const job2708 = new Job(2708, 0, "Biretta Books", "2021 Music Schedule", "16 page booklet, 5.5 x 8.5, St. John Cantius Church", "July 17, 2021", "August 6, 2021");
 job2708.completed_date = "August 1, 2021";
 job2708.total_hours = 8;
 job2708.rate_of_pay = 40;
@@ -65,7 +65,7 @@ job2708.date_paid = "August 9, 2011";
 jobs.push(job2708);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-const job2709 = new Job(2709, 1, "Publishing Management Associates", "First Things Flex 2.0", "Online Subs Form Creation", "July 16, 2021", "August 30, 2021");
+const job2709 = new Job(2709, 1, "Publishing Management Associates", "FT Flex 2.0", "Online Subs Form Creation for First Things", "July 16, 2021", "August 30, 2021");
 job2709.completed_date = "";
 job2709.total_hours = "";
 job2709.rate_of_pay = "";
@@ -74,13 +74,17 @@ job2709.date_paid = "";
 jobs.push(job2709);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-const job2710 = new Job(2710, 2, "St. John Cantius Church", "Posters", "", "August 6, 2021", "August 30, 2021");
+const job2710 = new Job(2710, 2, "Biretta Books", "Posters for St. John Cantius Church", "", "August 6, 2021", "August 30, 2021");
 job2710.completed_date = "";
 job2710.total_hours = "";
 job2710.rate_of_pay = "";
 job2710.date_billed = "";
 job2710.date_paid = "";
 jobs.push(job2710);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+const job2711 = new Job(2711, 3, "Publishing Management Associates", "FT House Ads", "Various small ads for print for First Things", "May 20, 2021", "August 30, 2021");
+jobs.push(job2711);
 
 //create array of invoice nubmers
 const invoice_numbers = jobs.map (function (job) {
