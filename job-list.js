b4262e85318d4ef581cde018bba5e87527f45b50
janQuart <janquart@fsj.org>
2021-09-18 16:05:42 -0500

    added hours and pdf to 2715

--- job-list.js
+++ job-list.js
@@ -180,8 +180,10 @@ job2715.hours_logged[2] = {Date: "Sept 11", Hours: .5, Note: "More art found"};
 job2715.hours_logged[3] = {Date: "Sept 14", Hours: .5, Note: "Formatting"};
 job2715.hours_logged[4] = {Date: "Sept 15", Hours: 1.5, Note: "Paintings, graphics"};
 job2715.hours_logged[5] = {Date: "Sept 16", Hours: 1, Note: "rework design layout"};
-job2715.hours_logged[6] = {Date: "Sept 17", Hours: 1, Note: "layout/scale pages"}
+job2715.hours_logged[6] = {Date: "Sept 17", Hours: 1, Note: "layout/scale pages"};
+job2715.hours_logged[6] = {Date: "Sept 18", Hours: 1.5, Note: "prepare fnl version/layout"};
 job2715.thumbnail = true;
+job2715.pdf = true;
 jobs.push(job2715);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
