935bdb17dd00b928e1f593df77d9ceb87abcc786
janQuart <janquart@fsj.org>
2021-10-23 16:16:35 -0500

    add letter to ordo

--- job-list.js
+++ job-list.js
@@ -216,6 +216,7 @@ job2717.hours_logged[6] = {Date: "Oct 19", Hours: 3.0, Note: "Date Layout"};
 job2717.hours_logged[7] = {Date: "Oct 20", Hours: 1, Note: "Date Layout"};
 job2717.hours_logged[8] = {Date: "Oct 21", Hours: 2, Note: "Finished Date Layout"};
 job2717.hours_logged[9] = {Date: "Oct 22", Hours: 4.5, Note: "Galleys finished"};
+job2717.hours_logged[10] = {Date: "Oct 23", Hours: 1, Note: "Add Francis Letter"};
 jobs.push(job2717);
 
 //end the jobs
