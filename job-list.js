a2d2c4421b7b01ca4de5e60eec975b42ee353603
janQuart <janquart@fsj.org>
2021-10-25 10:57:22 -0500

    update ordo hours Oct 25

--- job-list.js
+++ job-list.js
@@ -218,6 +218,7 @@ job2717.hours_logged[8] = {Date: "Oct 21", Hours: 2, Note: "Finished Date Layout
 job2717.hours_logged[9] = {Date: "Oct 22", Hours: 4.5, Note: "Galleys finished"};
 job2717.hours_logged[10] = {Date: "Oct 23", Hours: 1, Note: "Add Francis Letter"};
 job2717.hours_logged[11] = {Date: "Oct 24", Hours: 4.5, Note: "Finalize Interior"};
+job2717.hours_logged[12] = {Date: "Oct 25", Hours: 2, Note: "Applied corrections, proofing"};
 jobs.push(job2717);
 
 //end the jobs
