992ba5648f01f2b35332f8539c10b067f3f13c9d
janQuart <janquart@fsj.org>
2021-11-10 14:21:03 -0600

    updated and billed job 2716pma

--- job-list.js
+++ job-list.js
@@ -206,7 +206,9 @@ job2716.hours_logged[7] = {Date: "Oct 27", Hours: .2, Note: "Bachonors"};
 job2716.hours_logged[8] = {Date: "Oct 28", Hours: 1.25, Note: "Creation Book"};
 job2716.hours_logged[9] = {Date: "Nov 5", Hours: .2, Note: "FT 8th Day"};
 job2716.hours_logged[10] = {Date: "Nov 9", Hours: 1, Note: "C.M.H. Koenig"};
-job2716.completed_date = "November 9, 2021"; 
+job2716.hours_logged[11] = {Date: "Nov 10", Hours: .5, Note: "C.M.H Koenig"};
+job2716.completed_date = "November 10, 2021";
+job2716.date_billed = "November 10, 2021";
 job2716.thumbnail = true;
 jobs.push(job2716);
 
