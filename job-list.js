cedb990fa2aa95089a551ef8f75897349bf5f01f
janQuart <janquart@fsj.org>
2022-02-15 16:45:11 -0600

    had to push that last job

--- job-list.js
+++ job-list.js
@@ -268,5 +268,6 @@ jobs.push(job2719);
 
 const job2720 = new Job(2720, 12, "Publishing Management Associates", "PMA Small Ads 4", "Small ads created or reformatted", "February 15, 2022", "N/A");
 job2720.hours_logged[0] = {Date: "Feb 15", Hours: .15, Note: "Gideon"};
+jobs.push(job2720);
 
 
