4f8b60010371a073edb111f72cb670c5507bcb86
janQuart <janquart@fsj.org>
2022-05-03 11:16:53 -0500

    Cantius Book work

--- job-list.js
+++ job-list.js
@@ -284,6 +284,7 @@ job2719.hours_logged[7] = {Date: "Apr 25", Hours: 1, Note: "finished chapter txt
 job2719.hours_logged[8] = {Date: "Apr 25", Hours: 1, Note: "appendices"};
 job2719.hours_logged[9] = {Date: "Apr 26", Hours: 1, Note: "endnotes formatting"};
 job2719.hours_logged[10] = {Date: "Apr 27", Hours: 1, Note: "finalize draft to send around"};
+job2719.hours_logged[11] = {Date: "May 2", Hours: 1, Note: "proof passes 1"}
 jobs.push(job2719);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
