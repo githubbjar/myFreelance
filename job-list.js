fe121a671a0d5b1a1894318052a236285e96f60f
Jerry Janquart <jerryjanquart@Jerrys-iMac.attlocal.net>
2022-05-20 16:58:18 -0500

    begin illustrations

--- job-list.js
+++ job-list.js
@@ -296,7 +296,8 @@ job2719.hours_logged[7] = {Date: "Apr 25", Hours: 1, Note: "finished chapter txt
 job2719.hours_logged[8] = {Date: "Apr 25", Hours: 1, Note: "appendices"};
 job2719.hours_logged[9] = {Date: "Apr 26", Hours: 1, Note: "endnotes formatting"};
 job2719.hours_logged[10] = {Date: "Apr 27", Hours: 1, Note: "finalize draft to send around"};
-job2719.hours_logged[11] = {Date: "May 2", Hours: 1, Note: "proof passes 1"}
+job2719.hours_logged[11] = {Date: "May 2", Hours: 1, Note: "proof passes 1"};
+job2719.hours_logged[12] = {Date: "May 20", Hours: 1, Note: "Illustration layout"};
 jobs.push(job2719);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
